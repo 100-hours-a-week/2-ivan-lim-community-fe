546448dfed4cd596332c8fb8a25ef6bea5162a35
Lim <inlim@Limui-MacBookAir.local>
2024-12-16 15:07:57 +0900

    Add setting listening IP address for ec2

--- app.js
+++ app.js
@@ -97,6 +97,6 @@ app.get('/passModi', (req, res) => {
     );
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0',() => {
     console.log(`Server is running at http://localhost:${port}`);
 });
