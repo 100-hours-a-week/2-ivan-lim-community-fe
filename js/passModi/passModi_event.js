677a374a6337970ff5e9f50229fbfc737b414d66
Lim <inlim@Limui-MacBookAir.local>
2024-12-28 04:57:15 +0900

    fix : passModi 페이지 pwHelpertext 나타날 때 레이아웃 움직임

--- js/passModi/passModi_event.js
+++ js/passModi/passModi_event.js
@@ -26,7 +26,7 @@ $passwordInput.addEventListener('input', function() {
     }
     else if(!passwordValidChk($passwordInput.value)) {
         $pwHelperText.style.display = 'block';
-        $pwHelperText.textContent = '*비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.';
+        $pwHelperText.innerHTML = '*비밀번호는 8자 이상, 20자 이하이며,<br> 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.';
     }
     else {
         $pwHelperText.style.display = 'none';
