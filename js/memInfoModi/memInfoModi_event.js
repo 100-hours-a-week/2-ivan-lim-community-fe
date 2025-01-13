9ffd04036e8fd6362447591160406a10f57b8eaa
Lim <inlim@Limui-MacBookAir.local>
2025-01-14 02:08:52 +0900

    simple fix

--- js/memInfoModi/memInfoModi_event.js
+++ js/memInfoModi/memInfoModi_event.js
@@ -82,7 +82,7 @@ try{
         console.log(user);
         $profileImg.src = user.profileImgPath ? `${beOrigin}/userProfileImg/${user.profileImgPath}` : `${beOrigin}/userProfileImg/default.png`;
     }
-}catch(e){
+}catch(error){
     console.error('There was a problem with your fetch operation:', error);
 }
 
